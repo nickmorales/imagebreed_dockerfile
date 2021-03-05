8b23d484d90de33e13369036d1f986d1e6080fc1
nickmorales <nmorales3142@gmail.com>
2021-03-05 12:11:10 -0500

    docs

--- development/prepare.sh
+++ development/prepare.sh
@@ -4,8 +4,8 @@ mkdir repos
 mkdir repos_opencv
 
 # main code
-git clone https://github.com/solgenomics/cxgn-corelibs.git repos/cxgn-corelibs
-git clone https://github.com/nickmorales/sgn.git  repos/sgn
+git clone --branch topic/metadata_schema_fixes --depth 1 https://github.com/solgenomics/cxgn-corelibs.git repos/cxgn-corelibs
+git clone --branch master --depth 1 https://github.com/nickmorales/sgn.git  repos/sgn
 git clone https://github.com/solgenomics/Phenome.git repos/Phenome
 git clone https://github.com/solgenomics/rPackages.git repos/rPackages
 git clone https://github.com/solgenomics/biosource.git repos/biosource
