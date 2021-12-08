ef9d4f510c03df97f7287a9e4f7c9514c3d5e787
nickmorales <nmorales3142@gmail.com>
2021-12-08 12:53:45 -0500

    entrypoint

--- development/entrypoint.sh
+++ development/entrypoint.sh
@@ -15,14 +15,28 @@ then
         cd /home/production/cxgn/sgn
     fi
 
-    if [ "$RUN_DB_ONTOLOGY_UPDATES" == "TRUE" ]; then
+    if [ "$RUN_DB_ONTOLOGY_SGNSTAT_UPDATES" == "TRUE" ]; then
         perl /home/production/cxgn/Chado/chado/bin/gmod_load_cvterms.pl -H $DATABASE_HOST -D $DATABASE_NAME -u -s SGNSTAT -d Pg -r $DATABASE_USER -p $DATABASE_PASSWORD /home/production/cxgn/sgn/ontology/cxgn_statistics.obo
+        perl /home/production/cxgn/sgn/bin/ensure_only_one_variable_of_cvterm.pl -H $DATABASE_HOST -D $DATABASE_NAME -U $DATABASE_USER -P $DATABASE_PASSWORD
+    fi
+    if [ "$RUN_DB_ONTOLOGY_TIME_UPDATES" == "TRUE" ]; then
         perl /home/production/cxgn/Chado/chado/bin/gmod_load_cvterms.pl -H $DATABASE_HOST -D $DATABASE_NAME -u -s TIME -d Pg -r $DATABASE_USER -p $DATABASE_PASSWORD /home/production/cxgn/sgn/ontology/cxgn_time.obo
+        perl /home/production/cxgn/sgn/bin/ensure_only_one_variable_of_cvterm.pl -H $DATABASE_HOST -D $DATABASE_NAME -U $DATABASE_USER -P $DATABASE_PASSWORD
+    fi
+    if [ "$RUN_DB_ONTOLOGY_UO_UPDATES" == "TRUE" ]; then
         perl /home/production/cxgn/Chado/chado/bin/gmod_load_cvterms.pl -H $DATABASE_HOST -D $DATABASE_NAME -u -s UO -d Pg -r $DATABASE_USER -p $DATABASE_PASSWORD /home/production/cxgn/sgn/ontology/cxgn_units.obo
+        perl /home/production/cxgn/sgn/bin/ensure_only_one_variable_of_cvterm.pl -H $DATABASE_HOST -D $DATABASE_NAME -U $DATABASE_USER -P $DATABASE_PASSWORD
+    fi
+    if [ "$RUN_DB_ONTOLOGY_ISOL_UPDATES" == "TRUE" ]; then
         perl /home/production/cxgn/Chado/chado/bin/gmod_load_cvterms.pl -H $DATABASE_HOST -D $DATABASE_NAME -u -s ISOL -d Pg -r $DATABASE_USER -p $DATABASE_PASSWORD /home/production/cxgn/sgn/ontology/imagesol.obo
+        perl /home/production/cxgn/sgn/bin/ensure_only_one_variable_of_cvterm.pl -H $DATABASE_HOST -D $DATABASE_NAME -U $DATABASE_USER -P $DATABASE_PASSWORD
+    fi
+    if [ "$RUN_DB_ONTOLOGY_G2F_UPDATES" == "TRUE" ]; then
         perl /home/production/cxgn/Chado/chado/bin/gmod_load_cvterms.pl -H $DATABASE_HOST -D $DATABASE_NAME -u -s G2F -d Pg -r $DATABASE_USER -p $DATABASE_PASSWORD /home/production/cxgn/sgn/ontology/G2F.obo
+        perl /home/production/cxgn/sgn/bin/ensure_only_one_variable_of_cvterm.pl -H $DATABASE_HOST -D $DATABASE_NAME -U $DATABASE_USER -P $DATABASE_PASSWORD
+    fi
+    if [ "$RUN_DB_ONTOLOGY_ALF_UPDATES" == "TRUE" ]; then
         perl /home/production/cxgn/Chado/chado/bin/gmod_load_cvterms.pl -H $DATABASE_HOST -D $DATABASE_NAME -u -s ALF -d Pg -r $DATABASE_USER -p $DATABASE_PASSWORD /home/production/cxgn/sgn/ontology/Alfalfa.obo
-
         perl /home/production/cxgn/sgn/bin/ensure_only_one_variable_of_cvterm.pl -H $DATABASE_HOST -D $DATABASE_NAME -U $DATABASE_USER -P $DATABASE_PASSWORD
     fi
 fi
