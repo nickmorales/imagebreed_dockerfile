946d5a3f3cc61feec0eca74184a67ebb2c0c8908
nickmorales <nmorales3142@gmail.com>
2022-07-15 17:09:12 -0400

    9.3

--- development/entrypoint.sh
+++ development/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i s/localhost/$HOSTNAME/g /etc/slurm-llnl/slurm.conf
+sed -i s/localhost/$HOSTNAME/g /etc/slurm/slurm.conf
 /etc/init.d/postfix start
 /etc/init.d/munge start
 /etc/init.d/slurmctld start
@@ -42,9 +42,11 @@ then
 fi
 
 if [ "$MODE" == "DEVELOPMENT" ]; then
-	/home/production/cxgn/sgn/bin/sgn_server.pl --fork -r -d -p 8080
+    /home/production/cxgn/sgn/bin/sgn_server.pl --fork -r -d -p 8080
+elif [ "$MODE" == "BUILD_DEVELOPMENT" ]; then
+    sleep infinity
 else
-  /etc/init.d/sgn start
-  chmod 777 /var/log/sgn/error.log
-  tail -f /var/log/sgn/error.log
+    /etc/init.d/sgn start
+    chmod 777 /var/log/sgn/error.log
+    tail -f /var/log/sgn/error.log
 fi
